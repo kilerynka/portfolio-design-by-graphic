d9b01da09988c1f7f45be09e354ae32e22e34e99
Jakub Zakrzewicz <hipiseczek@gmail.com>
2022-03-18 21:57:55 +0100

    section three and footer on mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,19 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const nameGit = document.querySelector(".name-js");
+const demo = document.querySelector(".demo-js");
+const src = document.querySelector(".src-js");
+const descrip = document.querySelector(".description-js");
+
+fetch("https://api.github.com/users/kilerynka/repos")
+  .then((response) => response.json())
+  .then((resp) => {
+    const repository = resp.slice(5, 6);
+    for (let repo of repository) {
+      const { name, html_url, description } = repo;
+      nameGit.innerHTML = name;
+      src.setAttribute("href", html_url);
+      descrip.innerHTML = description;
+    }
+  });
