582308969033e0d35893cadd9cd90d8af71927a3
Jakub Zakrzewicz <hipiseczek@gmail.com>
2022-03-23 11:09:47 +0100

    og image added,actualization of readme, js code improved

--- readme.md
+++ readme.md
@@ -1,12 +1,14 @@
-![cover](https://cotenfrontend.pl/img/cover.png)
+![cover](https://kilerynka.io/og.jpg)
 
-# WTF Webpack Starter Kit 
+# Jakub Zakrzewicz
 
-Simple toolkit for your static websites development.
+My portfolio website
 
-## How to use it?
+## TOOLS
 
-Click on `Use this template` button to generate a new repository based on this starter kit. After that clone it to your computer, go to the project directory in console and type `npm install`.
+- Webpack starterkit
+- HTML + CSS + JS
+- Github API
 
 ## Available scripts
 
@@ -15,4 +17,3 @@ Click on `Use this template` button to generate a new repository based on this s
 `npm run build` - runs build process for production
 
 `npm run publish` - runs build process and publish the page using `gh-pages` branch
-
--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,50 @@
 import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
-
-const nameGit = document.querySelector(".name-js");
-const demo = document.querySelector(".demo-js");
-const src = document.querySelector(".src-js");
-const descrip = document.querySelector(".description-js");
-
 fetch("https://api.github.com/users/kilerynka/repos")
   .then((response) => response.json())
   .then((resp) => {
-    const repository = resp.slice(5, 6);
-    for (let repo of repository) {
-      const { name, html_url, description } = repo;
-      nameGit.innerHTML = name;
-      src.setAttribute("href", html_url);
-      descrip.innerHTML = description;
+    const container = document.querySelector(".sectionThree-js");
+    for (let repo of resp) {
+      const { name, html_url, description, homepage } = repo;
+      console.log(resp);
+      const template = `<article class="sectionThree__article">
+<div class="sectionThree__article-div">
+  <div class="threeDots"></div>
+</div>
+
+<div class="content">
+  <img class="content-img" src="img/Vector.png" alt="" />
+  <div class="wrap">
+    <section class="sectionGit">
+      <h2 class="sectionGit-h2">project:</h2>
+      <p class="title sectionGit-p name-js">${name}</p>
+    </section>
+    <section class="sectionGit">
+      <h2 class="sectionGit-h2">description:</h2>
+      <p class="description sectionGit-p description-js">
+      ${description}
+      </p>
+    </section>
+  </div>
+  <div class="wrap2">
+    <section class="sectionGit">
+      <h2 class="sectionGit-h2">demo:</h2>
+
+      <a
+        class="link sectionGit-p demo-js"
+        href="${homepage}"
+        >see here</a
+      >
+    </section>
+    <section class="sectionGit">
+      <h2 class="sectionGit-h2">github:</h2>
+      <a class="link sectionGit-p src-js" href="${html_url}"> source code </a>
+    </section>
+  </div>
+</div>
+</article>`;
+      if (description) {
+        container.innerHTML += template;
+      }
     }
   });
